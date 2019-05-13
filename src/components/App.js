4e8421bb6f0bd815023f6f8087051de6a1a09022
Keren Rogel <kerenrogel@gmail.com>
2019-05-13 16:06:06 +0800

    Set up react-app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component }from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  constructor() { //where we initialize our state
+    super(); //we are extending from the component class
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div className="App">
+
+      </div>
+    ); 
+  }
+}
+
+export default App;
